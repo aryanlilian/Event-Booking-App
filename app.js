0502915d9a94855b4d1d69904c45153a7e2c713a
Bularu Lilian <aryanlilian@gmail.com>
2021-06-11 21:26:12 +0300

    defined a temporary structure for an Event obj

--- app.js
+++ app.js
@@ -4,17 +4,34 @@ const { buildSchema } = require('graphql');
 const { graphqlHTTP } = require('express-graphql');
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 app.use(
     '/graphql',
     graphqlHTTP({
         schema: buildSchema(`
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
             type RootQuery {
-                events: [String!]!
+                events: [Event!]!
             }
         
             type RootMutation {
-                createEvent(name: String): String
+                createEvent(eventInput: EventInput): Event
             }
         
             schema {
@@ -24,11 +41,18 @@ app.use(
         `),
         rootValue: {
             events: () => {
-                return ['First events', 'Second event', 'Third event'];
+                return events;
             },
             createEvent: (args) => {
-                const eventName = args.name;
-                return eventName;
+                const event = {
+                    _id: Math.floor(Math.random() * 1000).toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: args.eventInput.price,
+                    date: args.eventInput.date
+                };
+                events.push(event);
+                return event;
             }
         },
         graphiql: true 
