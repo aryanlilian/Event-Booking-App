e137b018f826a8d9b9fdc89d8fee70de81470fe1
Bularu Lilian <aryanlilian@gmail.com>
2021-06-11 20:46:37 +0300

    configured a simple server with GraphQL

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: buildSchema(`
+            type RootQuery {
+                events: [String!]!
+            }
+        
+            type RootMutation {
+                createEvent(name: String): String
+            }
+        
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            events: () => {
+                return ['First events', 'Second event', 'Third event'];
+            },
+            createEvent: (args) => {
+                const eventName = args.name;
+                return eventName;
+            }
+        },
+        graphiql: true 
+    })
+  );
 
 app.get('/', (req, res) => {
     res.send("Hello world");
